ce02c8912aa3d5b3ac9c7c10d2ffc2f53c774b43
202030121 <bxbs0297@email.dealim.ac.kr>
2024-03-29 16:48:22 +0900

    3월 29일

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(13 + 1.43);
+import java.util.Scanner; 
+public class App { 
+    public static void main(String[] args) { 
 
-
-    }
-}
+        Scanner scanner= new Scanner(System.in); 
+        int intArray[]; 
+        intArray= new int[5]; 
+        int max=0; 
+        
+        System.out.print("양수 5개를 입력하세요 : "); 
+        for(int i=0; i<5; i++) { 
+            intArray[i] = scanner.nextInt(); 
+            if(intArray[i] > max) max = intArray[i]; 
+         }
+           System.out.print("가장큰수는" + max + "입니다."); scanner.close();
+     }
+           
+ }
--- src/Ch01.java
+++ src/Ch01.java
@@ -4,7 +4,7 @@ public class Ch01 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("월(1~12)을 입력하시오");
+        System.out.print("월(1~12)을 입력하시오 : ");
         int month = scanner.nextInt();
 
         if(3 <= month && month <= 5) {
@@ -15,6 +15,9 @@ public class Ch01 {
         }
         else if(9 <= month && month <= 11) {
             System.out.println("가을 입니다.");
+        }
+        else if(month >= 13) {
+            System.out.println("다시 입력 해 주세요 ");
         }
         else {
             System.out.println("겨울 입니다.");
