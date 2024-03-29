898c0ed7b3e344dbe85c56a1b469983838e1bca3
202030121 <bxbs0297@email.dealim.ac.kr>
2024-03-29 15:30:32 +0900

    3

---
+++ src/Ch01.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ch01 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("월(1~12)을 입력하시오");
+        int month = scanner.nextInt();
+
+        if(3 <= month && month <= 5) {
+            System.out.println("봄 입니다.");
+        }
+        else if(6 <= month && month <= 8) {
+            System.out.println("여름 입니다.");
+        }
+        else if(9 <= month && month <= 11) {
+            System.out.println("가을 입니다.");
+        }
+        else {
+            System.out.println("겨울 입니다.");
+        }
+
+    }
+}
