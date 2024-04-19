d527d32583f61d3f189782252afc71f29edfbe4a
202030121 <bxbs0297@email.dealim.ac.kr>
2024-04-19 15:46:54 +0900

    4월 19일

--- src/App.java
+++ src/App.java
@@ -1,18 +1,11 @@
-import java.util.Scanner; 
+import java2.Cal;
+
 public class App { 
     public static void main(String[] args) { 
 
-        Scanner scanner= new Scanner(System.in); 
-        int intArray[]; 
-        intArray= new int[5]; 
-        int max=0; 
-        
-        System.out.print("양수 5개를 입력하세요 : "); 
-        for(int i=0; i<5; i++) { 
-            intArray[i] = scanner.nextInt(); 
-            if(intArray[i] > max) max = intArray[i]; 
-         }
-           System.out.print("가장큰수는" + max + "입니다."); scanner.close();
+        Cal foo = new Cal();
+        System.out.println(foo.add(22,55));
+
      }
            
  }
